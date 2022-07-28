a889c80afd5a69beb914b42d380496c7f8a7825a
khalifa.twaha <khalifa.twaha@ega.go.tz>
2022-07-28 21:35:27 +0300

    learning Async functions in JS

---
+++ script.js
@@ -0,0 +1,61 @@
+'use strict';
+
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
+
+const renderError = function (msg) {
+  countriesContainer.insertAdjacentText('beforeend', msg);
+};
+
+const renderCountry = function (data, city = '') {
+  const header = '<h1 class = "heading-primary">Here you are!</h1>';
+  const html = `
+  <article class="country">
+          <img class="country__img" src="${data.flag}" />
+          <div class="country__data">
+            <h3 class="country__name">${data.name}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>📍</span>${city}</p>
+            <p class="country__row"><span>👫</span>${(
+              data.population / 1000000
+            ).toFixed(1)} people</p>
+            <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+            <p class="country__row"><span>💰</span>${
+              data.currencies[0].code
+            }</p>
+          </div>
+        </article>
+  `;
+  document.body.insertAdjacentHTML('afterbegin', header);
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+const getPosition = function () {
+  return new Promise(function (resolve, reject) {
+    navigator.geolocation.getCurrentPosition(resolve, reject);
+  });
+};
+
+const whereAmI = async function () {
+  // Geolocation
+  const pos = await getPosition();
+  const { latitude: lat, longitude: lng } = pos.coords;
+
+  // Reverse geocoding
+  const resGeo = await fetch(
+    `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}&localityLanguage=en`
+  );
+  const dataGeo = await resGeo.json();
+  const city = dataGeo.city;
+
+  // Country data
+  const res = await fetch(
+    `https://restcountries.com/v2/name/${dataGeo.countryName}`
+  );
+  const data = await res.json();
+  renderCountry(data[0], city);
+  console.log(data[0]);
+};
+
+btn.addEventListener('click', whereAmI);
